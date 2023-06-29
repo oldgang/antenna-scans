021eb940065bc90f52cc28a7eee5e84f7d381ab6
oldgang <iceman16999@gmail.com>
2023-06-29 11:10:48 +0200

    preparing for flask

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 from tabulate import tabulate
-scan_data = list()
-filtered_data = list()
-with open("C:\\Users\\icema\\OneDrive\\Pulpit\\scan.txt", 'r') as infile:
-    for line in infile:
-        split = line.split(',')
-        filtered = list()
-        filtered.append(split[0])
-        filtered.append(split[1])
-        filtered.append(split[2])
-        filtered.append(split[3])
-        filtered_data.append(filtered)
 
-#nice printing and sorting
-filtered_data.sort(key=lambda row: row[3])
-print (tabulate(filtered_data, headers=["MAC", "SSID", "Channel", "Signal Strength"]))
+def readData(path):
+    scan_data = list()
+    filtered_data = list()
+    with open(path, 'r') as infile:
+        for line in infile:
+            split = line.split(',')
+            filtered = list()
+            filtered.append(split[0])
+            filtered.append(split[1])
+            filtered.append(split[2])
+            filtered.append(split[3])
+            filtered_data.append(filtered)
+
+    #nice printing and sorting
+    filtered_data.sort(key=lambda row: row[3])
+    print (tabulate(filtered_data, headers=["MAC", "SSID", "Channel", "Signal Strength"]))
+
+if __name__ == "__main__":
+    path = "C:\\Users\\icema\\OneDrive\\Pulpit\\scan.txt"
+    readData(path)
