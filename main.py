fd1d92bd276722581774fc0249258ffa529177b5
oldgang <iceman16999@gmail.com>
2023-06-29 11:11:53 +0200

    preparing for flask p2

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from tabulate import tabulate
 
 def readData(path):
-    scan_data = list()
-    filtered_data = list()
+    filteredData = list()
     with open(path, 'r') as infile:
         for line in infile:
             split = line.split(',')
@@ -11,12 +10,13 @@ def readData(path):
             filtered.append(split[1])
             filtered.append(split[2])
             filtered.append(split[3])
-            filtered_data.append(filtered)
+            filteredData.append(filtered)
 
     #nice printing and sorting
-    filtered_data.sort(key=lambda row: row[3])
-    print (tabulate(filtered_data, headers=["MAC", "SSID", "Channel", "Signal Strength"]))
+    filteredData.sort(key=lambda row: row[3])
+    return filteredData
 
 if __name__ == "__main__":
     path = "C:\\Users\\icema\\OneDrive\\Pulpit\\scan.txt"
-    readData(path)
+    filteredData = readData(path)
+    print (tabulate(filteredData, headers=["MAC", "SSID", "Channel", "Signal Strength"]))
